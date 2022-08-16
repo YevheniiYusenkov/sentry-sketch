80cb3e7eb2a88cf825b512246e347270f54cc1c8
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-16 15:44:27 +0300

    Implemented test case

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
+
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<any> {
+    const transaction = Sentry.startTransaction({
+      op: 'test',
+      name: 'My First Test Transaction',
+    });
+
+    setTimeout(() => {
+      try {
+        throw new BadRequestException('Error for sentry.');
+      } catch (e) {
+        Sentry.captureException(e);
+      } finally {
+        transaction.finish();
+      }
+    }, 99);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import * as Tracing from '@sentry/tracing';
+import * as Sentry from '@sentry/node';
+
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  Sentry.init({
+    dsn: process.env.SENTRY_URI,
+    tracesSampleRate: 1.0,
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
